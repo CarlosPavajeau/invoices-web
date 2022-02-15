0203a99b847ed073bf270ddfe60cd7e7c34ae956
cantte <cantte098@gmail.com>
2022-02-14 21:27:39 -0500

    feat: show all invoices in main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Invoice } from './model/Invoice'
+import { InvoicesService } from './services/invoices.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'invoices';
+export class AppComponent implements OnInit {
+  invoices: Invoice[] = []
+
+  title = 'invoices'
+
+  public constructor(private invoicesService: InvoicesService) {
+  }
+
+  ngOnInit(): void {
+    this.invoicesService.fetchAll().subscribe(invoices => {
+      this.invoices = invoices
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatTableModule } from '@angular/material/table'
+import { HttpClientModule } from '@angular/common/http';
+import { InvoicesTableComponent } from './components/invoices-table/invoices-table.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InvoicesTableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
